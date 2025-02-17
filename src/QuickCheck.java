8df772637b1fdc1e4107e18d4fddff6a04891f9a
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-02-17 11:30:48 -0800

    Method headers

--- src/QuickCheck.java
+++ src/QuickCheck.java
@@ -1,3 +1,16 @@
 public class QuickCheck {
-  p
+
+  // Print all strings longer than 7 characters in a tree
+  // Each string should be printed on a separate line, in pre-order
+  // If the node is null, print nothing
+  // You can assume there are no null strings in the values of the nodes.
+  public void printLongWords(TreeNode<String> node) {
+
+  }
+
+  // Return the sum of all odd-valued nodes in the tree
+  // If the node is null, return 0
+  public int oddSum(TreeNode<String> node) {
+    return -1;
+  }
 }
